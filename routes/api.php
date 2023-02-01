9aecb9b3bb08cb7ae9bc3ab19737fbf05f6c64d9
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-02-01 17:07:22 +0100

    set api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/posts', 'Api\PostController@index');
+
+Route::namespace('Api')->prefix('/posts')->group(function(){
+    Route::get('PostController@index');
+});
