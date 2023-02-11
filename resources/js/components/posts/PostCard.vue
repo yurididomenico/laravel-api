36614d39ab06c2ca8209344c23eadc186625ed5b
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-02-11 14:54:12 +0100

    .

---
+++ resources/js/components/posts/PostCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3></h3>
+    </div>
+</template>
+
+<script>
+export default
+{
+    name: 'PostsList',
+    props: ['posts', 'isLoading']
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- resources/js/views/pages/posts/PostShow.vue
+++ resources/js/views/pages/posts/PostShow.vue
@@ -1,15 +1,20 @@
 <template>
     <div>
         <h1>Pagina post singolo</h1>
+        <PostCard />
     </div>
 </template>
 
 <script>
 // import PostsList from "../../posts/PostsList.vue"
+
+import PostCard from "../../../components/posts/PostCard.vue"
+
 export default
 {
     name: 'PostShow',
     components: {
+        PostCard
     },
     data()
     {
